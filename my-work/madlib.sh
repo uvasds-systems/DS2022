b8a81232d471e950d4f09dfa0860fce6efd3c614
Dailin Li <165076853+dylanlidailin@users.noreply.github.com>
2024-09-21 17:17:41 -0400

    Add a madlib to github

---
+++ my-work/madlib.sh
@@ -0,0 +1,19 @@
+#!
+
+clear
+echo "Let's build a mad-lib!"
+
+read -p "1. Name an unusual sport: " NOUN1
+read -p "2. Name a verb that desribes what would you do when you are upset: " NOUN2
+read -p "3. Who's your high school English teacher: " NOUN3
+read -p "4. What's your favorite food at o'hill: " NOUN4
+read -p "5. Give me an adverb: " NOUN5
+read -p "6. Name the smallest piece of furniture in your house: " NOUN6
+read -p "7. Name a big city in the US: " NOUN7
+read -p "8. Tell me what's your favorite song: " NOUN8
+
+echo "It was a Sunday afternoon. My piano was complaining how he's sad he's the only piano that doesn't know $NOUN1.
+He was about to $NOUN2 because I hushed him as I heard $NOUN3 calling my name downstairs.
+She's my piano teacher and she liked $NOUN4, which was intimidating.
+$NOUN5, I hid under the $NOUN6 and booked a flight to $NOUN7 to skip the class.
+Then I heard my piano singing $NOUN8, my least favorite song. That's when I noticed I was in a dream."
