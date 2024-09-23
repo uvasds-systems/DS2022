e174b115b20503caedb5f4f3d703d54cc1f8b8f1
ziyajasani <144265578+ziyajasani@users.noreply.github.com>
2024-09-22 22:54:40 -0400

    Update github-events.py

--- my-work/lab3/github-events.py
+++ my-work/lab3/github-events.py
@@ -1,1 +1,17 @@
+#!/opt/anaconda3/bin/python3
 
+import os
+import json
+import requests
+
+GHUSER = os.getenv('ziyajasani')
+
+url = "https://api.github.com/users/{0}/events'.format(GUSER)
+
+r = json.loads(requests.get(url).text)
+
+for x in r[:5]:
+  event = x['type'] + ' :: ' + x['repo']['name']
+  print(event)
+
+print(r)
