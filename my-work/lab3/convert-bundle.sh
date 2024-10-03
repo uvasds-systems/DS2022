8bd68e069b0a8f492a830c768b34493edda0fbaa
Ziya Jasani <ziyajasani@Ziyas-MacBook-Pro.local>
2024-10-03 15:21:19 -0400

    stage for commit

--- my-work/lab3/convert-bundle.sh
+++ my-work/lab3/convert-bundle.sh
@@ -2,13 +2,16 @@
 
 curl https://s3.amazonaws.com/ds2002-resources/labs/lab3-bundle.tar.gz
 
-tar -xzf lab3-bundle.tsv
+tar -xzf lab3-bundle.tar.gz
 
-awk '!/^[[:space:]]*$/' lab3-bundle.tar.gz
+awk '!/^[[:space:]]*$/' lab3_data.tsv > my_file.tsv
 
-awk 'BEGIN { FS="\t"; OFS="," } {$1=$1; print}' lab3-bundle.tar.gz > lab3-bundle.tar.gz.csv
+awk 'BEGIN { FS="\t"; OFS="," } {$1=$1; print}' my_file.tsv > 
+my_file.csv
 
-linecount=$(wc - l lab3-bundle.tar.gz.csv)
+
+linecount=$(wc - l my_file.csv)
 echo "The number of lines in the .csv file is $linecount."
 
-tar -cvf converted-archive.tar.gz lab3-bundle.tar.gz.csv
+tar -cvf converted-archive.tar.gz my_file.csv
+
